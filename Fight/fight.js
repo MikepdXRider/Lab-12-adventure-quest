c084cf354a363d2b6bddf8d8f7bdb68cc4cf6c0a
Dylan Floyd <floyd800@gmail.com>
2021-09-02 16:53:22 -0700

    add initial enemy rendering

--- Fight/fight.js
+++ Fight/fight.js
@@ -9,3 +9,32 @@ if (Number.isNaN(fightId) || typeof fightId !== 'number') {
 const fight = fightData.find((fight) => {
     return fight.id === fightId;
 });
+
+//get all elements
+const enemyNameEl = document.querySelector('#fight-div > h3');
+const promptEl = document.querySelector('#fight-div > h4');
+const enemyImg = document.querySelector('#enemy-img');
+const optionsDiv = document.querySelector('#options-div');
+const resultsDiv = document.querySelector('#results-div');
+//grab form elements
+const form = document.querySelector('#options-form');
+const option1Text = document.querySelector('#option-1-text');
+const option2Text = document.querySelector('#option-2-text');
+const option3Text = document.querySelector('#option-3-text');
+
+enemyNameEl.textContent = fight.enemyName;
+promptEl.textContent = fight.prompt;
+enemyImg.src = fight.image;
+option1Text.textContent = fight.options[0].text;
+option2Text.textContent = fight.options[1].text;
+option3Text.textContent = fight.options[2].text;
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const formdata = new FormData(form);
+    let selectedOptionIndex = formdata.get('option-radio');
+    console.log(selectedOptionIndex);
+    console.log(fight.options[selectedOptionIndex].resultText);
+    optionsDiv.removeChild(form);
+    resultsDiv.textContent = fight.options[selectedOptionIndex].resultText;
+});
