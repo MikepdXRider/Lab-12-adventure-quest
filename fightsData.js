7b6306aeedba47252c852c5727674985babc97ad
Dylan Floyd <floyd800@gmail.com>
2021-09-02 15:22:53 -0700

    add initial fightData structure

---
+++ fightsData.js
@@ -0,0 +1,89 @@
+export const fightData = [
+    {
+        id: 1,
+        image: 'http://www.placekitten.com/200/200',
+        enemyName: 'bob',
+        prompt: 'Bob says your shirt makes you look fat. How do you respond?',
+        options: [
+            {
+                text: 'Yeah well your shirt makes you look fat too.',
+                hpEffect: 5,
+                fameEffect: 5,
+                startsFight: true,
+                resultText: 'NA, this will be the result from rock paper scissors instead'
+            },
+            {
+                text: 'Ah man, yeah I\'ve been gaining some weight lately.',
+                hpEffect: 0,
+                fameEffect: 10,
+                startsFight: false,
+                resultText: 'Bob laughs at you and smacks your belly, you lose 5 fame'
+            },
+            {
+                text: 'Uhh, okay.',
+                hpEffect: 0,
+                fameEffect: 0,
+                startsFight: false,
+                resultText: 'Bob is confused by your neutrality and goes back to being annoying in general'
+            }
+        ]
+    },
+    {
+        id: 2,
+        image: 'http://www.placekitten.com/200/200',
+        enemyName: 'bob2',
+        prompt: 'Bob says your shirt makes you look fat. How do you respond?',
+        options: [
+            {
+                text: 'Yeah well your shirt makes you look fat too.',
+                hpEffect: 5,
+                fameEffect: 5,
+                startsFight: true,
+                resultText: 'NA, this will be the result from rock paper scissors instead'
+            },
+            {
+                text: 'Ah man, yeah I\'ve been gaining some weight lately.',
+                hpEffect: 0,
+                fameEffect: 10,
+                startsFight: false,
+                resultText: 'Bob laughs at you and smacks your belly, you lose 5 fame'
+            },
+            {
+                text: 'Uhh, okay.',
+                hpEffect: 0,
+                fameEffect: 0,
+                startsFight: false,
+                resultText: 'Bob is confused by your neutrality and goes back to being annoying in general'
+            }
+        ]
+    },
+    {
+        id: 3,
+        image: 'http://www.placekitten.com/200/200',
+        enemyName: 'bob3',
+        prompt: 'Bob says your shirt makes you look fat. How do you respond?',
+        options: [
+            {
+                text: 'Yeah well your shirt makes you look fat too.',
+                hpEffect: 5,
+                fameEffect: 5,
+                startsFight: true,
+                resultText: 'NA, this will be the result from rock paper scissors instead'
+            },
+            {
+                text: 'Ah man, yeah I\'ve been gaining some weight lately.',
+                hpEffect: 0,
+                fameEffect: 10,
+                startsFight: false,
+                resultText: 'Bob laughs at you and smacks your belly, you lose 5 fame'
+            },
+            {
+                text: 'Uhh, okay.',
+                hpEffect: 0,
+                fameEffect: 0,
+                startsFight: false,
+                resultText: 'Bob is confused by your neutrality and goes back to being annoying in general'
+            }
+        ]
+    },
+];
