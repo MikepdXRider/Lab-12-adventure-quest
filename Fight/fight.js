8c264dc2489b9ccfaf1cb8c04e210d24fa2b3029
Dylan Floyd <floyd800@gmail.com>
2021-09-03 09:47:11 -0700

    implement stat effect function and return to dojo link

--- Fight/fight.js
+++ Fight/fight.js
@@ -1,7 +1,12 @@
 import { fightData } from '../fightsData.js';
+import { handleStatEffect } from './handle-stat-effect.js';
 
 //get the fight data
 const searchParams = new URLSearchParams(window.location.search);
+const stringyId = searchParams.get('id');
+if (typeof stringyId === 'undefined') {
+    alert('Something went wrong, please go back to the dojo page');
+}
 const fightId = Number(searchParams.get('id'));
 if (Number.isNaN(fightId) || typeof fightId !== 'number') {
     alert('Something went wrong, please go back to the dojo page');
@@ -16,6 +21,7 @@ const promptEl = document.querySelector('#fight-div > h4');
 const enemyImg = document.querySelector('#enemy-img');
 const optionsDiv = document.querySelector('#options-div');
 const resultsDiv = document.querySelector('#results-div');
+const returnLink = document.querySelector('#return-link');
 //grab form elements
 const form = document.querySelector('#options-form');
 const option1Text = document.querySelector('#option-1-text');
@@ -28,6 +34,10 @@ enemyImg.src = fight.image;
 option1Text.textContent = fight.options[0].text;
 option2Text.textContent = fight.options[1].text;
 option3Text.textContent = fight.options[2].text;
+
+function showReturnLink() {
+    returnLink.className = '';
+}
 
 //lazy rps game
 const rps = ['Rock', 'Paper', 'Scissors'];
@@ -52,11 +62,13 @@ const playRoundOfRPS = (selectedOption) => {
         losses++;
     }
     if (wins === 3) {
-        resultsDiv.textContent = `You beat ${fight.enemyName}!! You gained ${selectedOption.hpEffect} hp and ${selectedOption.fameEffect} fame.`;
-        //update user data
+        resultsDiv.textContent = `You beat ${fight.enemyName}! You gained ${selectedOption.hpEffect} hp and ${selectedOption.fameEffect} fame.`;
+        showReturnLink();
+        handleStatEffect(selectedOption.hpEffect, selectedOption.fameEffect);
     } else if (losses === 3) {
-        resultsDiv.textContent = `You lost to ${fight.enemyName}!! You lost ${selectedOption.hpEffect} hp and ${selectedOption.fameEffect} fame.`;
-        //update user data
+        resultsDiv.textContent = `You lost to ${fight.enemyName}! You lost ${selectedOption.hpEffect} hp and ${selectedOption.fameEffect} fame.`;
+        showReturnLink();
+        handleStatEffect((-1 * selectedOption.hpEffect), (-1 * selectedOption.fameEffect));
     } else {
         resultsDiv.textContent = `You played ${userPlay} and ${fight.enemyName} played ${enemyPlay}. ${results}. Wins: ${wins}, Losses: ${losses}`;
         setTimeout(() => { playRoundOfRPS(selectedOption); }, 1500);
@@ -71,13 +83,15 @@ function playRockPaperScissors(selectedOption) {
 form.addEventListener('submit', e => {
     e.preventDefault();
     const formdata = new FormData(form);
-    let selectedOptionIndex = formdata.get('option-radio');
-    let selectedOption = fight.options[selectedOptionIndex];
+    const selectedOptionIndex = formdata.get('option-radio');
+    const selectedOption = fight.options[selectedOptionIndex];
     optionsDiv.removeChild(form);
     resultsDiv.textContent = fight.options[selectedOptionIndex].resultText;
     if (selectedOption.startsFight) {
         playRockPaperScissors(selectedOption);
     } else {
         resultsDiv.textContent = `hp += ${selectedOption.hpEffect}, fame += ${selectedOption.fameEffect}`;
+        handleStatEffect(selectedOption.hpEffect, selectedOption.fameEffect);
+        showReturnLink();
     }
 });
