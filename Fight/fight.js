6637a72327a800517ca4e0ea396b2a9e11648d18
Dylan Floyd <floyd800@gmail.com>
2021-09-03 16:43:08 -0700

    working rock paper scissors game

--- Fight/fight.js
+++ Fight/fight.js
@@ -17,7 +17,7 @@ const fight = fightData.find((fight) => {
     return fight.id === fightId;
 });
 
-//Get all elements
+//Grab all elements
 const enemyNameEl = document.querySelector('#fight-div > h3');
 const promptEl = document.querySelector('#fight-div > h4');
 const enemyImg = document.querySelector('#enemy-img');
@@ -25,12 +25,16 @@ const optionsDiv = document.querySelector('#options-div');
 const resultsDiv = document.querySelector('#results-div');
 const returnLink = document.querySelector('#return-link');
 const userStatsDiv = document.querySelector('#stats-container');
+const rpsDiv = document.querySelector('#rps-div');
 
-//Grab form elements
-const form = document.querySelector('#options-form');
+//Grab fight options form elements
+const optionsForm = document.querySelector('#options-form');
 const option1Text = document.querySelector('#option-1-text');
 const option2Text = document.querySelector('#option-2-text');
 const option3Text = document.querySelector('#option-3-text');
+
+//Grab rock paper scissors form
+const rpsForm = document.querySelector('#rps-form');
 
 //Render fight data
 enemyNameEl.textContent = fight.enemyName;
@@ -59,6 +63,7 @@ function handleFightCompletion(resultText, hpEffect, fameEffect) {
     resultsDiv.textContent = resultText;
     handleStatEffect(hpEffect, fameEffect);
     updateUserStatsDisplay();
+    rpsDiv.className = 'hidden';
     showReturnLink();
 }
 
@@ -69,12 +74,13 @@ updateUserStatsDisplay();
 const rps = ['Rock', 'Paper', 'Scissors'];
 let wins = 0;
 let losses = 0;
+let selectedResponse; //the response the user selected.
 
 //This function calls itself recursively. It will stop
 //when wins or losses reaches 3
-const playRockPaperScissors = (selectedOption) => {
+const playRockPaperScissors = (selectedObject) => {
     //Randomly assign picks to user and enemy
-    const userPlay = rps[(Math.floor(Math.random() * 3))];
+    const userPlay = selectedObject;
     const enemyPlay = rps[(Math.floor(Math.random() * 3))];
 
     //Determine result of round
@@ -96,41 +102,50 @@ const playRockPaperScissors = (selectedOption) => {
     //Handle the result of the round
     if (wins === 3) {
         //The user won
-        const resultText = `You beat ${fight.enemyName}! You gained ${selectedOption.hpEffect} hp and ${selectedOption.fameEffect} fame.`;
-        handleFightCompletion(resultText, selectedOption.hpEffect, selectedOption.fameEffect);
+        const resultText = `You beat ${fight.enemyName}! You gained ${selectedResponse.hpEffect} hp and ${selectedResponse.fameEffect} fame.`;
+        handleFightCompletion(resultText, selectedResponse.hpEffect, selectedResponse.fameEffect);
     } else if (losses === 3) {
         //The enemy won
-        const resultText = `You lost to ${fight.enemyName}! You lost ${selectedOption.hpEffect} hp and ${selectedOption.fameEffect} fame.`;
-        handleFightCompletion(resultText, selectedOption.hpEffect, selectedOption.fameEffect);
+        const resultText = `You lost to ${fight.enemyName}! You lost ${selectedResponse.hpEffect} hp and ${selectedResponse.fameEffect} fame.`;
+        handleFightCompletion(resultText, selectedResponse.hpEffect, selectedResponse.fameEffect);
     } else {
         //No result yet, play another round
         resultsDiv.textContent = `You played ${userPlay} and ${fight.enemyName} played ${enemyPlay}. ${results}. Wins: ${wins}, Losses: ${losses}`;
-        setTimeout(() => { playRockPaperScissors(selectedOption); }, 1500);
     }
 };
 
 //Starts a game of rock paper scissors
-function startRockPaperScissors(selectedOption) {
-    resultsDiv.textContent = `${fight.enemyName} challenges you to a game of rock, paper, scissors ... of death.`;
-    setTimeout(() => { playRockPaperScissors(selectedOption); }, 1500);
+function startRockPaperScissors() {
+    resultsDiv.textContent = `${fight.enemyName} challenges you to a game of rock, paper, scissors ... of death. Choose your move carefully:`;
+    rpsDiv.className = ''; //remove the hidden class
 }
 
 //Handle the user selecting an option
-form.addEventListener('submit', e => {
+optionsForm.addEventListener('submit', e => {
     e.preventDefault();
 
     //Get the data for the user selected response
-    const formdata = new FormData(form);
+    const formdata = new FormData(optionsForm);
     const selectedOptionIndex = formdata.get('option-radio');
-    const selectedOption = fight.options[selectedOptionIndex];
+    selectedResponse = fight.options[selectedOptionIndex];
 
     //Update the page to show the results of the response.
-    optionsDiv.removeChild(form);
+    optionsDiv.className = 'hidden';
     resultsDiv.textContent = fight.options[selectedOptionIndex].resultText;
-    if (selectedOption.startsFight) {
-        startRockPaperScissors(selectedOption);
+    if (selectedResponse.startsFight) {
+        startRockPaperScissors(selectedResponse);
     } else {
-        const resultText = `Effect on hp: ${selectedOption.hpEffect}, Effect on fame: ${selectedOption.fameEffect}`;
-        handleFightCompletion(resultText, selectedOption.hpEffect, selectedOption.fameEffect);
+        const resultText = `Effect on hp: ${selectedResponse.hpEffect}, Effect on fame: ${selectedResponse.fameEffect}`;
+        handleFightCompletion(resultText, selectedResponse.hpEffect, selectedResponse.fameEffect);
     }
+});
+
+//Handle the user clicking the choose button for rps
+rpsForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //Get the data for the user selected rps object.
+    const formData = new FormData(rpsForm);
+    const selectedObject = formData.get('rps-radio');
+    playRockPaperScissors(selectedObject);
 });
